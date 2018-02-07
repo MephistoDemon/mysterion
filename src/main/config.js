5d10517e2db574aefe6ccf1c4e77976d87d49372
Waldz <valdas@mysterium.network>
2018-02-07 17:18:06 +0200

    Make path variables clear

--- src/main/config.js
+++ src/main/config.js
@@ -2,15 +2,20 @@
 
 import {app} from 'electron'
 import path from 'path'
+import {Config as MysteriumConfig} from '../libraries/mysterium-client'
 
 export default function (global) {
-  if (process.env.NODE_ENV !== 'development') {
-    let appPath = app.getAppPath()
-    global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
-    global.__mysteriumClientBin = path.resolve(appPath, '../../bin/mysterium_client')
-  } else {
-    let appPath = path.resolve(__dirname, '../../') // path from this file
-    global.__static = require('path').join(appPath, '/static').replace(/\\/g, '\\\\')
-    global.__mysteriumClientBin = path.resolve(appPath + '/bin/mysterium_client')
+  let appPath = app.getAppPath()
+  if (process.env.NODE_ENV === 'development') {
+    // path from this file
+    appPath = path.resolve(__dirname, '../../')
   }
+
+  global.__static = path.join(appPath, 'static').replace(/\\/g, '\\\\')
+
+  global.__mysteriumClientConfig = new MysteriumConfig(
+    path.join(appPath, 'bin', 'mysterium_client'),
+    app.getPath('temp'),
+    app.getPath('userData')
+  )
 }
