db07e2e26fb5882744703d2bae5a7ea7d5b54fa1
Andrej Novikov <jazzaiman@gmail.com>
2018-02-22 02:36:11 +0200

    format statistics
    Signed-off-by: Andrej Novikov <jazzaiman@gmail.com>

--- src/renderer/components/StatsDisplay.vue
+++ src/renderer/components/StatsDisplay.vue
@@ -2,17 +2,17 @@
   <div class="stats__container">
     <div class="stats__block">
       <div class="stats__label">TIME</div>
-      <div class="stats__value">{{connection.stats.duration}}</div>
+      <div class="stats__value">{{connection.stats.duration | secToString}}</div>
       <div class="stats__unit">H:M:S</div>
     </div>
     <div class="stats__block">
       <div class="stats__label">RECEIVED</div>
-      <div class="stats__value">{{connection.stats.bytesReceived}}</div>
+      <div class="stats__value">{{connection.stats.bytesReceived | bytesToMB}}</div>
       <div class="stats__unit">MB</div>
     </div>
     <div class="stats__block">
       <div class="stats__label">SENT</div>
-      <div class="stats__value">{{connection.stats.bytesSent}}</div>
+      <div class="stats__value">{{connection.stats.bytesSent | bytesToMB}}</div>
       <div class="stats__unit">MB</div>
     </div>
   </div>
@@ -23,6 +23,19 @@
     name: 'stats-display',
     props: {
       connection: {}
+    },
+    filters: {
+      secToString (val) {
+        const h = Math.floor(val / 3600)
+        const m = Math.floor((val - h * 3600) / 60)
+        const s = (val - h * 3600 - m * 60)
+        return `${h}:${m}:${s}`
+      },
+      bytesToMB (val) {
+        if (val) {
+          return (val / (1024 ** 2)).toLocaleString(undefined, {maximumFractionDigits: 2})
+        }
+      }
     }
   }
 </script>
