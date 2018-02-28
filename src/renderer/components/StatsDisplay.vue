e42a20490ca42c63f347f47aef2a03aec80558ec
Andrej Novikov <jazzaiman@gmail.com>
2018-02-28 16:28:08 +0200

    statistics with dynamic units
    Signed-off-by: Andrej Novikov <jazzaiman@gmail.com>

--- src/renderer/components/StatsDisplay.vue
+++ src/renderer/components/StatsDisplay.vue
@@ -7,32 +7,44 @@
     </div>
     <div class="stats__block">
       <div class="stats__label">RECEIVED</div>
-      <div class="stats__value">{{connection.stats.bytesReceived | bytesToMB}}</div>
-      <div class="stats__unit">MB</div>
+      <div class="stats__value">{{received.value}}</div>
+      <div class="stats__unit">{{received.suffix}}</div>
     </div>
     <div class="stats__block">
       <div class="stats__label">SENT</div>
-      <div class="stats__value">{{connection.stats.bytesSent | bytesToMB}}</div>
-      <div class="stats__unit">MB</div>
+      <div class="stats__value">{{sent.value}}</div>
+      <div class="stats__unit">{{sent.suffix}}</div>
     </div>
   </div>
 </template>
 
 <script>
+  import size from 'file-size'
+
   export default {
     name: 'stats-display',
     props: {
       connection: {}
     },
+    computed: {
+      received: (vm) => {
+        const sizeInfo = size(vm.connection.stats.bytesReceived).calculate()
+        return {value: sizeInfo.fixed, suffix: sizeInfo.suffix}
+      },
+      sent: (vm) => {
+        const sizeInfo = size(vm.connection.stats.bytesSent).calculate()
+        return {value: sizeInfo.fixed, suffix: sizeInfo.suffix}
+      }
+    },
     filters: {
       secToString (val) {
-        const h = Math.floor(val / 3600)
-        const m = Math.floor((val - h * 3600) / 60)
-        const s = (val - h * 3600 - m * 60)
+        let h = Math.floor(val / 3600)
+        h = h > 9 ? h : '0' + h
+        let m = Math.floor((val - h * 3600) / 60)
+        m = m > 9 ? m : '0' + m
+        let s = (val - h * 3600 - m * 60)
+        s = s > 9 ? s : '0' + s
         return `${h}:${m}:${s}`
-      },
-      bytesToMB (val) {
-        return (val / (1024 ** 2)).toLocaleString(undefined, {maximumFractionDigits: 2})
       }
     }
   }
--- test/unit/specs/components/StatsDisplay.spec.js
+++ test/unit/specs/components/StatsDisplay.spec.js
@@ -24,24 +24,25 @@ describe('StatsDisplay', () => {
   it('renders and displays stats', () => {
     const vm = mount(initialConnectionState)
     const els = vm.$el.querySelectorAll('.stats__value')
-    expect(els[0].textContent).to.contain('0')
-    expect(els[1].textContent).to.contain('0')
-    expect(els[2].textContent).to.contain('0')
+    expect(els[0].textContent).to.eql('00:00:00')
+    expect(els[1].textContent).to.eql('0.00')
+    expect(els[2].textContent).to.eql('0.00')
   })
-})
 
-describe('statsDisplay Filter', () => {
-  const vm = mount({
-    stats: {
-      duration: 103425,
-      bytesReceived: 1232133, // 1.17505 MB
-      bytesSent: 123321 // 0.117608 MB
-    }
-  })
-  it('displays stats properly', () => {
+  it('displays stats formatting', () => {
+    const vm = mount({
+      stats: {
+        duration: 13325,
+        bytesReceived: 1232133, // 1.17505 MB
+        bytesSent: 123321 // 0.117608 MB
+      }
+    })
     const els = vm.$el.querySelectorAll('.stats__value')
-    expect(els[0].textContent).to.eql('28:43:45')
+    expect(els[0].textContent).to.eql('03:42:05')
     expect(els[1].textContent).to.eql('1.18')
-    expect(els[2].textContent).to.eql('0.12')
+    expect(els[2].textContent).to.eql('120.43')
+    const unitEls = vm.$el.querySelectorAll('.stats__unit')
+    expect(unitEls[1].textContent).to.eql('MiB')
+    expect(unitEls[2].textContent).to.eql('KiB')
   })
 })
