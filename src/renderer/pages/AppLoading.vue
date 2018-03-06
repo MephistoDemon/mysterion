d5ce02d89b90c4470efc3e24206337cda795134b
Ignas Bernotas <ignas@iber.lt>
2018-03-06 16:36:15 +0200

    removed unneeded placeholder component code

--- src/renderer/pages/AppLoading.vue
+++ src/renderer/pages/AppLoading.vue
@@ -1,24 +1,3 @@
 <template>
     <div></div>
 </template>
-<script>
-  import AppModal from '@/partials/AppModal'
-  import {mapGetters} from 'vuex'
-
-  export default {
-    data () {
-      return {
-        status: {
-          message: 'App is loading, please wait'
-        }
-      }
-    },
-    components: {
-      AppModal
-    },
-    computed: {
-      ...mapGetters(['overlayError'])
-    },
-    name: 'home'
-  }
-</script>
