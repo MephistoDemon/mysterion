1bed63492e62dbf0f166238aece59862b9227324
Andrej Novikov <jazzaiman@gmail.com>
2018-03-20 14:11:21 +0200

    fix mysterium-client test
    Signed-off-by: Andrej Novikov <jazzaiman@gmail.com>

--- test/unit/specs/libraries/mysterium-client/standalone/process.spec.js
+++ test/unit/specs/libraries/mysterium-client/standalone/process.spec.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-expressions */
-import Process from '../../../../../../src/libraries/mysterium-client/standalone/process'
+import Process, {logLevel} from '../../../../../../src/libraries/mysterium-client/standalone/process'
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import configInjector from 'inject-loader!../../../../../../src/app/mysterion-config'
 import {ChildProcess} from 'child_process'
@@ -21,22 +21,34 @@ const config = configInjector({
 
 describe('Standalone Process', () => {
   let process, tequilapi
+  const logs = []
   const port = 4055
   before(async () => {
     tequilapi = tequilAPI(`http://127.0.0.1:${port}`)
     process = new Process(config)
-    await delay(100)
+    process.start(port)
+    process.on(logLevel.LOG, data => logs.push(data))
+    await delay(500)
   })
 
-  it('spawns in less than 100ms without errors', () => {
-    process.start(port)
+  after(() => {
+    process.stop()
+  })
+
+  it('spawns in less than 500ms without errors', () => {
     expect(process.child).to.be.instanceOf(ChildProcess)
   })
+
+  it('sends log data to callback on()', () => {
+    expect(logs.pop()).to.include('Api started on: ' + port)
+  })
+
   it('responds to healthcheck with uptime', async () => {
     const res = await tequilapi.healthCheck()
     expect(res.uptime).to.be.ok
     expect(res.version).to.be.ok
   })
+
   it('responds to healthcheck with version string', async () => {
     const res = await tequilapi.healthCheck()
     expect(res.version).to.include.all.keys(['branch', 'commit', 'buildNumber'])
