74fca21241dd68d9f5e39523c2f3bd85f83208d6
Andrej Novikov <jazzaiman@gmail.com>
2018-03-23 14:43:53 +0200

    plain text terms
    Signed-off-by: Andrej Novikov <jazzaiman@gmail.com>

--- src/main/index.js
+++ src/main/index.js
@@ -3,12 +3,13 @@
 import MysterionFactory from '../app/mysterion'
 import MysterionConfig from '../app/mysterion-config'
 import bugReporter from '../app/bug-reporting'
+import {terms, version as termsVersion} from '../../mysterion-terms'
 
 global.__version = process.env.MYSTERION_VERSION
 global.__sentryURL = process.env.SENTRY.publicURL
 global.__static = MysterionConfig.staticDirectoryPath
 
-const mysterion = MysterionFactory(MysterionConfig, 'Cool terms', '1.0')
+const mysterion = MysterionFactory(MysterionConfig, terms, termsVersion)
 mysterion.run()
 
 bugReporter.main.install()
--- src/renderer/pages/Terms.vue
+++ src/renderer/pages/Terms.vue
@@ -1,20 +1,21 @@
 <template>
-    <div class="terms">
-        <div class="terms-content">
-            <h1>Terms and conditions <small>v{{ termsAndConditions.version }}</small></h1>
-            <div><p>{{ termsAndConditions.content }}</p></div>
-        </div>
-        <div class="terms-actions">
-            <a href="#" class="control__action btn" @click.prevent="accept">Accept and launch Mysterion</a>
-            <a href="#" class="control__action btn btn-danger" @click.prevent="decline">Decline</a>
-        </div>
+  <div class="terms">
+    <div class="terms-content">
+      <div style="height:10vh;border-bottom:1px solid #eee"></div>
+      <div class="terms-box">
+        <pre>{{ termsAndConditions.content }}</pre>
+      </div>
     </div>
+    <div class="terms-actions">
+      <a href="#" class="control__action btn" @click.prevent="accept">Accept and launch Mysterion</a>
+      <a href="#" class="control__action btn btn-danger" @click.prevent="decline">Decline</a>
+    </div>
+  </div>
 </template>
 <script>
   import {ipcRenderer} from 'electron'
   import {mapGetters} from 'vuex'
   import communication from '../../app/communication'
-
   export default {
     name: 'terms',
     methods: {
@@ -32,29 +33,37 @@
 </script>
 
 <style lang="less">
-    .terms {
-        height: 100vh;
-        .terms-content {
-            padding: 20px;
-            margin-bottom: 50px;
-        }
-        .terms-actions {
-            position: fixed;
-            bottom: 0;
-            padding: 10px;
-            width: 100%;
-            text-align: center;
-            background-color: #fff;
-            border-top: 1px solid #eee;
-            .btn {
-                font-size: 1.5rem;
-                max-width: 30rem;
-                width: auto;
-                &.btn-danger {
-                    background-color: #a60404;
-                    border-color: #a60404;
-                }
-            }
-        }
+  .terms {
+    height: 100vh;
+    .terms-content {
+      margin-bottom: 50px;
     }
+    .terms-box {
+      height: 80vh;
+      overflow: scroll;
+      pre {
+        white-space: pre-line;
+        padding: 1rem 8rem;
+        max-width: 100vw;
+      }
+    }
+    .terms-actions {
+      position: fixed;
+      bottom: 0;
+      padding: 10px;
+      width: 100%;
+      text-align: center;
+      background-color: #fff;
+      border-top: 1px solid #eee;
+      .btn {
+        font-size: 1.5rem;
+        max-width: 30rem;
+        width: auto;
+        &.btn-danger {
+          background-color: #a60404;
+          border-color: #a60404;
+        }
+      }
+    }
+  }
 </style>
