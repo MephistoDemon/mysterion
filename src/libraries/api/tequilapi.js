06c7473789d059f87856a6f015f71f64deb464f0
Andrej Novikov <jazzaiman@gmail.com>
2018-04-10 13:39:27 +0300

    default parameter timeout on connection.ip() and healthCheck()
    Signed-off-by: Andrej Novikov <jazzaiman@gmail.com>

--- src/libraries/api/tequilapi.js
+++ src/libraries/api/tequilapi.js
@@ -28,13 +28,13 @@ export default function Constructor (teqAddr = 'http://127.0.0.1:4050') {
       }),
       disconnect: async () => axioAdapter.delete(conPath),
       status: async () => axioAdapter.get(conPath),
-      ip: async ({timeout}) => {
+      ip: async ({timeout = 0} = {}) => {
         const res = await axioAdapter.get(conPath + '/ip', {timeout})
         return res.ip
       },
       statistics: async () => axioAdapter.get(conPath + '/statistics')
     },
-    healthCheck: async ({timeout}) => axioAdapter.get(healthCheckPath, {timeout}),
+    healthCheck: async ({timeout = 0} = {}) => axioAdapter.get(healthCheckPath, {timeout}),
     stop: async () => axioAdapter.post(stopPath),
     __axio: teqAxio // we need this for mocking
   }
