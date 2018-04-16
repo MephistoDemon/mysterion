1d394add0332572a2974c90366c8b2548c2473e8
Donatas Kucinskas <donce.lt@gmail.com>
2018-04-16 16:39:04 +0300

    Mark which communication messages needs to be moved to communication

--- src/app/communication/index.js
+++ src/app/communication/index.js
@@ -1,13 +1,15 @@
 // TODO: rename to messages.js
 export default {
+  CONNECTION_STATUS_CHANGED: 'connection.status.changed',
   MYSTERIUM_CLIENT_LOG: 'mysterium.client.log',
   IDENTITY_SET: 'identity.set',
+
+  // TODO: refactor these message to be used in communication
   RENDERER_LOADED: 'renderer.loaded',
   TERMS_REQUESTED: 'terms.requested',
   TERMS_ANSWERED: 'terms.answered',
   TERMS_ACCEPTED: 'terms.accepted',
   APP_START: 'app.start',
   APP_ERROR: 'app.error',
-  HEALTHCHECK: 'healthcheck',
-  CONNECTION_STATUS_CHANGED: 'connection.status.changed'
+  HEALTHCHECK: 'healthcheck'
 }
