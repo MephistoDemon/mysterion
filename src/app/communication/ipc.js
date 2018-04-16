1e4f629f32f64e56aa481765e8dbc495968a5177
Donatas Kucinskas <donce.lt@gmail.com>
2018-04-16 10:49:07 +0300

    Improve Ipc interface

--- src/app/communication/ipc.js
+++ src/app/communication/ipc.js
@@ -1,5 +1,5 @@
 // @flow
 export interface Ipc {
   send (channel: string, data?: mixed): void,
-  on (channel: string, callback: Function): void
+  on (channel: string, callback: (data?: mixed) => void): void
 }
