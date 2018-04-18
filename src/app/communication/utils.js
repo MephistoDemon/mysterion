4494de12b252d84fa6bdc8aac21184002ecb8ae6
Donatas Kucinskas <donce.lt@gmail.com>
2018-04-18 11:16:50 +0300

    Refactor communication utils

--- src/app/communication/utils.js
+++ src/app/communication/utils.js
@@ -1,5 +1,13 @@
 // @flow
-function waitForMessage (subscriber: (() => void) => void): Promise<void> {
+type Callback = () => void
+
+/**
+ * Subscribes for specific event and resolves when first event is received.
+ *
+ * @param subscriber - function to subscribe for specific event
+ * @returns {Promise<any>}
+ */
+function waitForMessage (subscriber: (Callback) => void): Promise<void> {
   return new Promise((resolve) => {
     subscriber(() => {
       resolve()
