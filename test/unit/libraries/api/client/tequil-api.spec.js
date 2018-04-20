216f68788eb9a8ae041bbba34298dfca4259f8c3
Waldz <valdas@mysterium.network>
2018-04-20 15:00:26 +0300

    Test TequilAPI.GetProposals()

--- test/unit/libraries/api/client/tequil-api.spec.js
+++ test/unit/libraries/api/client/tequil-api.spec.js
@@ -3,57 +3,62 @@ import FakeAdapter from '../../../../../src/libraries/api/client/adapters/fake-a
 import ProposalDto from '../../../../../src/libraries/api/client/dto/proposal'
 import TequilApi from '../../../../../src/libraries/api/client/tequil-api'
 
-describe('TequilApi', () => {
+describe('tequilAPI', () => {
   describe('client', () => {
-    const proposals = {
-      proposals: [{
-        id: 1,
-        providerId: '0x0',
-        serviceType: 'openvpn',
-        serviceDefinition: {
-          locationOriginate: {
-            asn: '',
-            country: 'NL'
-          }
-        }
-      }, {
-        id: 1,
-        providerId: '0x1',
-        serviceType: 'openvpn',
-        serviceDefinition: {
-          locationOriginate: {
-            asn: '',
-            country: 'LT'
-          }
-        }
-      }]
-    }
+    let api
+    let adapter
 
-    const healthcheck = {
-      uptime: '1h10m',
-      process: 1111,
-      version: {
-        commit: '0bcccc',
-        branch: 'master',
-        buildNumber: '001'
-      }
-    }
-
-    const adapter = new FakeAdapter()
-    adapter.setProposalsResponse(proposals)
-    adapter.setHealthcheckResponse(healthcheck)
-
-    const api = new TequilApi(adapter)
+    before(() => {
+      adapter = new FakeAdapter()
+      api = new TequilApi(adapter)
+    })
 
     it('.getProposals returns new proposal instances', async () => {
-      const response = await api.getProposals()
-      expect(response[0]).to.deep.equal(new ProposalDto(proposals.proposals[0]))
-      expect(response[1]).to.deep.equal(new ProposalDto(proposals.proposals[1]))
+      const response = {
+        proposals: [{
+          id: 1,
+          providerId: '0x0',
+          serviceType: 'openvpn',
+          serviceDefinition: {
+            locationOriginate: {
+              asn: '',
+              country: 'NL'
+            }
+          }
+        }, {
+          id: 1,
+          providerId: '0x1',
+          serviceType: 'openvpn',
+          serviceDefinition: {
+            locationOriginate: {
+              asn: '',
+              country: 'LT'
+            }
+          }
+        }]
+      }
+      adapter.setProposalsResponse(response)
+
+      const proposals = await api.getProposals()
+      expect(proposals).to.have.lengthOf(2)
+      expect(proposals[0]).to.deep.equal(new ProposalDto(response.proposals[0]))
+      expect(proposals[1]).to.deep.equal(new ProposalDto(response.proposals[1]))
     })
 
     it('.healthcheck', async () => {
-      const response = await api.healthCheck()
-      expect(response).to.deep.equal(healthcheck)
+      const response = {
+        uptime: '1h10m',
+        process: 1111,
+        version: {
+          commit: '0bcccc',
+          branch: 'master',
+          buildNumber: '001'
+        }
+      }
+      adapter.setHealthcheckResponse(response)
+
+      const healthcheck = await api.healthCheck()
+      expect(healthcheck).to.deep.equal(response)
     })
   })
 })
