3d79ae0b8bda1855914616eb64d244d785de1de6
Waldz <valdas@mysterium.network>
2018-05-07 16:05:19 +0300

    Indroduce service unavailable error

---
+++ src/libraries/api/client/tequilapi-error.js
@@ -0,0 +1,51 @@
+// @flow
+
+const errorCodes = {
+  CONNECTION_ABORTED_ERROR_CODE: 'ECONNABORTED'
+}
+
+const httpResponseCodes = {
+  CLIENT_CLOSED_REQUEST: 499
+}
+
+class TequilapiError extends Error {
+  original: Error
+
+  constructor (original: Error) {
+    super()
+    super.name = this.constructor.name
+    super.message = original.message
+    super.stack = original.stack
+
+    this.original = original
+  }
+
+  isNetworkError (): boolean {
+    return this.message === 'Network Error'
+  }
+
+  isTimeoutError (): boolean {
+    if (!this.original.code) {
+      return false
+    }
+    return this.original.code === errorCodes.CONNECTION_ABORTED_ERROR_CODE
+  }
+
+  isRequestClosedError (): boolean {
+    return hasHttpStatus(this.original, httpResponseCodes.CLIENT_CLOSED_REQUEST)
+  }
+
+  isServiceUnavailable (): boolean {
+    return hasHttpStatus(this.original, httpResponseCodes.SERVICE_UNAVAILABLE)
+  }
+}
+
+function hasHttpStatus (error: Error, expectedStatus: number): boolean {
+  if (!error.response) {
+    return false
+  }
+
+  return error.response.status === expectedStatus
+}
+
+export default TequilapiError
