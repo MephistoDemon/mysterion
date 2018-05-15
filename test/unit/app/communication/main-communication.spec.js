65f6227def8ad3f8663f53e93f0150209655a669
Donatas Kucinskas <donce.lt@gmail.com>
2018-05-15 11:42:46 +0300

    Add missing specs for MainCommunication

--- test/unit/app/communication/main-communication.spec.js
+++ test/unit/app/communication/main-communication.spec.js
@@ -4,6 +4,7 @@ import messages from '../../../../src/app/communication/messages'
 import FakeMessageBus from '../../../helpers/fakeMessageBus'
 import { describe, beforeEach, it } from '../../../helpers/dependencies'
 import { CallbackRecorder } from '../../../helpers/utils'
+import ProposalDTO from '../../../../src/libraries/mysterium-tequilapi/dto/proposal'
 
 // TODO: add specs for new methods
 
@@ -24,6 +25,80 @@ describe('MainCommunication', () => {
       communication.sendMysteriumClientLog(data)
 
       expect(fakeMessageBus.lastChannel).to.eql(messages.MYSTERIUM_CLIENT_LOG)
+      expect(fakeMessageBus.lastData).to.eql(data)
+    })
+  })
+
+  describe('sendProposals', () => {
+    it('sends message to bus', () => {
+      const data = [new ProposalDTO({ id: 1 })]
+      communication.sendProposals(data)
+
+      expect(fakeMessageBus.lastChannel).to.eql(messages.PROPOSALS_UPDATE)
+      expect(fakeMessageBus.lastData).to.eql(data)
+    })
+  })
+
+  describe('sendConnectionCancelRequest', () => {
+    it('sends message to bus', () => {
+      communication.sendConnectionCancelRequest()
+
+      expect(fakeMessageBus.lastChannel).to.eql(messages.CONNECTION_CANCEL)
+    })
+  })
+
+  describe('sendConnectionRequest', () => {
+    it('sends message to bus', () => {
+      const data = { providerId: '0x123' }
+      communication.sendConnectionRequest(data)
+
+      expect(fakeMessageBus.lastChannel).to.eql(messages.CONNECTION_REQUEST)
+      expect(fakeMessageBus.lastData).to.eql(data)
+    })
+  })
+
+  describe('sendTermsRequest', () => {
+    it('sends message to bus', () => {
+      const data = { content: 'Cool terms' }
+      communication.sendTermsRequest(data)
+
+      expect(fakeMessageBus.lastChannel).to.eql(messages.TERMS_REQUESTED)
+      expect(fakeMessageBus.lastData).to.eql(data)
+    })
+  })
+
+  describe('sendTermsAccepted', () => {
+    it('sends message to bus', () => {
+      communication.sendTermsAccepted()
+
+      expect(fakeMessageBus.lastChannel).to.eql(messages.TERMS_ACCEPTED)
+    })
+  })
+
+  describe('sendAppStart', () => {
+    it('sends message to bus', () => {
+      communication.sendAppStart()
+
+      expect(fakeMessageBus.lastChannel).to.eql(messages.APP_START)
+    })
+  })
+
+  describe('sendAppError', () => {
+    it('sends message to bus', () => {
+      const data = { message: 'Message', hint: 'Hint', fatal: false }
+      communication.sendAppError(data)
+
+      expect(fakeMessageBus.lastChannel).to.eql(messages.APP_ERROR)
+      expect(fakeMessageBus.lastData).to.eql(data)
+    })
+  })
+
+  describe('sendHealthCheck', () => {
+    it('sends message to bus', () => {
+      const data = { status: true }
+      communication.sendHealthCheck(data)
+
+      expect(fakeMessageBus.lastChannel).to.eql(messages.HEALTHCHECK)
       expect(fakeMessageBus.lastData).to.eql(data)
     })
   })
@@ -58,6 +133,27 @@ describe('MainCommunication', () => {
       fakeMessageBus.triggerOn(messages.RENDERER_LOADED)
 
       expect(callbackRecorder.invoked).to.eql(true)
+    })
+  })
+
+  describe('onProposalUpdateRequest', () => {
+    it('receives message from bus', () => {
+      communication.onProposalUpdateRequest(callbackRecorder.getCallback())
+      fakeMessageBus.triggerOn(messages.PROPOSALS_UPDATE)
+
+      expect(callbackRecorder.invoked).to.eql(true)
+    })
+  })
+
+  describe('onTermsAnswered(', () => {
+    it('receives message from bus', () => {
+      communication.onTermsAnswered(callbackRecorder.getCallback())
+
+      const data = { answer: true }
+      fakeMessageBus.triggerOn(messages.TERMS_ANSWERED, data)
+
+      expect(callbackRecorder.invoked).to.eql(true)
+      expect(callbackRecorder.data).to.eql(data)
     })
   })
 })
