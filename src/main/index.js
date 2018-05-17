7a740b1b2ca1b80d3d7ffad0a61ebb737b0beffd
Waldz <valdas@mysterium.network>
2018-05-17 09:06:26 +0300

    Minimal main entrypoint file

--- src/main/index.js
+++ src/main/index.js
@@ -1,10 +1,3 @@
-// @flow
-'use strict'
-
 import dependencies from './dependencies'
 
-const mysterionConfig = dependencies.get('mysterionApplication.config')
-global.__static = mysterionConfig.staticDirectory
-
-const mysterion = dependencies.get('mysterionApplication')
-mysterion.run()
+dependencies.get('mysterionApplication').run()
