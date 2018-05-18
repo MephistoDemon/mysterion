f0c222594575ba9c1dee5c2842428e807c94dfed
Donatas Kucinskas <donce.lt@gmail.com>
2018-05-18 11:12:00 +0300

    Extract logic out of TequilapiClientError

--- src/libraries/mysterium-tequilapi/client-error.js
+++ src/libraries/mysterium-tequilapi/client-error.js
@@ -22,23 +22,39 @@ class TequilapiClientError extends Error {
   }
 
   isNetworkError (): boolean {
-    return this.message === 'Network Error'
+    return isNetworkError(this)
   }
 
   isTimeoutError (): boolean {
-    if (!this.original.code) {
-      return false
-    }
-    return this.original.code === errorCodes.CONNECTION_ABORTED_ERROR_CODE
+    return isTimeoutError(this.original)
   }
 
   isRequestClosedError (): boolean {
-    return hasHttpStatus(this.original, httpResponseCodes.CLIENT_CLOSED_REQUEST)
+    return isRequestClosedError(this.original)
   }
 
   isServiceUnavailableError (): boolean {
-    return hasHttpStatus(this.original, httpResponseCodes.SERVICE_UNAVAILABLE)
+    return isServiceUnavailableError(this.original)
   }
+}
+
+function isNetworkError (error: Error): boolean {
+  return error.message === 'Network Error'
+}
+
+function isTimeoutError (error: Error): boolean {
+  if (!error.code) {
+    return false
+  }
+  return error.code === errorCodes.CONNECTION_ABORTED_ERROR_CODE
+}
+
+function isRequestClosedError (error: Error): boolean {
+  return hasHttpStatus(error, httpResponseCodes.CLIENT_CLOSED_REQUEST)
+}
+
+function isServiceUnavailableError (error: Error): boolean {
+  return hasHttpStatus(error, httpResponseCodes.SERVICE_UNAVAILABLE)
 }
 
 function hasHttpStatus (error: Error, expectedStatus: number): boolean {
