d2f38db41950d90f708addf9198dbf9155baf3ab
Andrej Novikov <jazzaiman@gmail.com>
2018-05-24 15:29:10 +0300

    cleanup

--- src/main/dependencies/modules/user-settings.js
+++ src/main/dependencies/modules/user-settings.js
@@ -17,26 +17,17 @@
 
 // @flow
 import type {Container} from '../../../app/di'
-import {UserSettingsStore} from '../../../app/userSettings'
+import {UserSettingsStore} from '../../../app/user-settings/user-settings-store'
 import {join} from 'path'
 
 const userSettingsFilename = 'userSettings.json'
 
 function bootstrap (container: Container) {
   container.factory(
-    'userSettingsPath',
+    'userSettingsStore',
     ['mysterionApplication.config'],
     (mysterionConfig) => {
-      return join(mysterionConfig.userDataDirectory, userSettingsFilename)
-    }
-  )
-
-  container.factory(
-    'userSettingsStore',
-    ['userSettingsPath'],
-    (userSettingsPath) => {
-      const userSettings = new UserSettingsStore(userSettingsPath)
-      return userSettings
+      return new UserSettingsStore(join(mysterionConfig.userDataDirectory, userSettingsFilename))
     }
   )
 }
--- test/unit/app/userSettings.spec.js
+++ test/unit/app/userSettings.spec.js
@@ -16,7 +16,7 @@
  */
 
 // @flow
-import {UserSettingsStore} from '../../../src/app/userSettings'
+import {UserSettingsStore} from '../../../src/app/user-settings/user-settings-store'
 import {describe, expect, it, after, before} from '../../helpers/dependencies'
 import {tmpdir} from 'os'
 import {join} from 'path'
@@ -32,7 +32,7 @@ describe('UserSettingsStore', () => {
       unlinkSync(saveSettingsPath)
     })
 
-    it('exports a valid json on save()', async () => {
+    it('exports a valid json file', async () => {
       const userSettingsStore = new UserSettingsStore(saveSettingsPath)
       userSettingsStore.set({showDisconnectNotifications: false})
       await userSettingsStore.save()
@@ -44,9 +44,9 @@ describe('UserSettingsStore', () => {
     it('throws error if save() fails on invalid path to file', async () => {
       const userSettingsStore = new UserSettingsStore(invalidPath)
       userSettingsStore.set({showDisconnectNotifications: false})
-      const err = await capturePromiseError(userSettingsStore.save())
+      const error = await capturePromiseError(userSettingsStore.save())
 
-      expect(err instanceof Error).to.be.true
+      expect(error).to.be.an.instanceOf(Error)
     })
   })
 
@@ -74,21 +74,14 @@ describe('UserSettingsStore', () => {
       const userSettingsStore = new UserSettingsStore(loadSettingsPath)
       await userSettingsStore.load()
 
-      expect(userSettingsStore.settings).to.be.eql({showDisconnectNotifications: false})
+      expect(userSettingsStore.get()).to.be.eql({showDisconnectNotifications: false})
     })
 
-    it('throws error if load() fails on invalid path to file', async () => {
+    it('throws error if it fails on invalid path to file', async () => {
       const userSettingsStore = new UserSettingsStore(invalidPath)
       const error = await capturePromiseError(userSettingsStore.load())
 
-      expect(error instanceof Error).to.be.true
-    })
-
-    it('throws on load() if invalid Json file is read', async () => {
-      const userSettingsStore = new UserSettingsStore(invalidJsonPath)
-      const error = await capturePromiseError(userSettingsStore.load())
-
-      expect(error instanceof Error).to.be.true
+      expect(error).to.be.an.instanceOf(Error)
     })
   })
 })
