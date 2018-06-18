4f7d2c26a46a3fc07cf54336cc61903f203e2af8
Andrej Novikov <jazzaiman@gmail.com>
2018-06-18 11:45:28 +0300

    allow console usage
    Signed-off-by: Andrej Novikov <jazzaiman@gmail.com>

--- src/app/logger.js
+++ src/app/logger.js
@@ -19,12 +19,10 @@
 
 class Logger {
   info (...data: Array<any>): void {
-    // eslint-disable-next-line no-console
     console.info(...data)
   }
 
   error (...data: Array<any>): void {
-    // eslint-disable-next-line no-console
     console.error(...data)
   }
 }
