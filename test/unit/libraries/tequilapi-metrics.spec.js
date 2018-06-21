24ad3530febff4bbad833d39138f340c27ff3de5
mipo47 <mipo3815@gmail.com>
2018-06-21 14:22:58 +0300

    simplify test

--- test/unit/libraries/tequilapi-metrics.spec.js
+++ test/unit/libraries/tequilapi-metrics.spec.js
@@ -3,44 +3,24 @@
 
 import {expect, beforeEach, describe, it} from '../../helpers/dependencies'
 import TequilapiClientWithMetrics from '../../../src/libraries/tequilapi-metrics'
-import axios from 'axios/index'
-import AxiosAdapter from '../../../src/libraries/mysterium-tequilapi/adapters/axios-adapter'
-import MockAdapter from 'axios-mock-adapter'
 import {BugReporterMetrics, METRICS} from '../../../src/app/bug-reporting/bug-reporter-metrics'
-import HttpTequilapiClient from '../../../src/libraries/mysterium-tequilapi/client'
-import NodeHealthcheckDTO from '../../../src/libraries/mysterium-tequilapi/dto/node-healthcheck'
+import EmptyTequilapiClientMock from '../renderer/store/modules/empty-tequilapi-client-mock'
 
 describe('HttpTequilapiClientWithMetrics', () => {
   let api
   let metrics
   let apiMetrics
-  let mock
 
   beforeEach(() => {
-    const axioInstance = axios.create()
-    const http = new AxiosAdapter(axioInstance)
-    api = new HttpTequilapiClient(http)
+    api = new EmptyTequilapiClientMock()
     metrics = new BugReporterMetrics()
     apiMetrics = new TequilapiClientWithMetrics(api, metrics)
-    mock = new MockAdapter(axioInstance)
   })
 
   describe('healthcheck()', () => {
     it('returns response', async () => {
-      const response = {
-        uptime: '1h10m',
-        process: 1111,
-        version: {
-          commit: '0bcccc',
-          branch: 'master',
-          buildNumber: '001'
-        }
-      }
-      mock.onGet('healthcheck').reply(200, response)
-
       expect(metrics.get(METRICS.HealthCheckTime)).to.be.undefined
-      const healthcheck = await apiMetrics.healthCheck()
-      expect(healthcheck).to.deep.equal(new NodeHealthcheckDTO(response))
+      await apiMetrics.healthCheck()
       expect(metrics.get(METRICS.HealthCheckTime)).to.be.not.undefined
     })
   })
